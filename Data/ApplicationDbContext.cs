87a8a1db1a473f5c283f211f6e92130b0a5386ad
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2025-05-23 20:47:05 -0500

    init

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace pc3_progra.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
